f559ffc833ae3b54297a93a2c5cfbad93057c8a4
Prince Baghel <pkbghl90@gmil.com>
2021-09-19 06:26:31 +0530

    completed 1.8

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,14 +1,31 @@
 
 import React, { useState } from 'react';
+
 //Button Component******************************
 const Button = ({ onClick, text }) => (
-    <button onClick={onClick}>
-      {text}
-    </button>
+  <button onClick={onClick}>
+    {text}
+  </button>
 )
 //Line Component*******************************
-const Line = ({text, count}) => <p>{text} {count}</p>
+const Line = ({ text, count }) => <p>{text} {count}</p>
+//showing stastics using stats object**********
+const Statistics = (props) => {
 
+  const { goodText, goodCount, neutralText, neutralCount, badText, badCount, allText, allCount, averageText, averageCount, positiveText, positiveCount } = props.data;
+
+
+  return (
+    <>
+      <Line text={goodText} count={goodCount} />
+      <Line text={neutralText} count={neutralCount} />
+      <Line text={badText} count={badCount} />
+      <Line text={allText} count={allCount} />
+      <Line text={averageText} count={averageCount} />
+      <Line text={positiveText} count={positiveCount} />
+    </>
+  )
+}
 //App Component
 const App = () => {
 
@@ -25,17 +42,17 @@ const App = () => {
   //Event Hnadler functions***********************
   const goodHandler = () => {
     setGood(good + 1);
-    setAll(all+1);
-    setAvgSum(avgSum+1);
-    const avg = avgSum/all;
+    setAll(all + 1);
+    setAvgSum(avgSum + 1);
+    const avg = avgSum / all;
     setAverage(avg);
-    const annn = (good/all) * 100;
+    const annn = (good / all) * 100;
     setPositive(annn)
-    
+
   }
   const neutralHandler = () => {
     setNeutral(neutral + 1);
-    setAll(all+1);
+    setAll(all + 1);
     setAvgSum(avgSum);
     const avg = avgSum / all;
     setAverage(avg);
@@ -44,15 +61,30 @@ const App = () => {
   }
   const badHandler = () => {
     setBad(bad + 1);
-    setAll(all+1);
-    setAvgSum(avgSum-1);
+    setAll(all + 1);
+    setAvgSum(avgSum - 1);
     const avg = avgSum / all;
     setAverage(avg);
     const annn = (good / all) * 100;
     setPositive(annn)
   }
 
-  
+  const statsObject = {
+    goodText: "good",
+    goodCount: good,
+    neutralText: "neutral",
+    neutralCount: neutral,
+    badText: "bad",
+    badCount: bad,
+    allText: "all",
+    allCount: all,
+    averageText: "average",
+    averageCount: average,
+    positiveText: "positive",
+    positiveCount: positive
+  }
+
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -65,14 +97,10 @@ const App = () => {
       <Button
         onClick={badHandler}
         text="bad" />
-        <br />
-        <h1>statistics</h1>
-        <Line text="good" count={good} />
-        <Line text="neutral" count={neutral} />
-        <Line text="bad" count={bad} />
-        <Line text="all" count={all} />
-        <Line text="average" count={average} />
-        <Line text="positive" count={positive} />
+      <br />
+      <h1>statistics</h1>
+      <Statistics
+        data={statsObject} />
     </div>
   );
 }
