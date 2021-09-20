fa0b1b0606ff33fdb1713d6ed7a98f4e1a48eeca
Prince Baghel <pkbghl90@gmil.com>
2021-09-20 12:27:00 +0530

    completed 1.13

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -3,34 +3,49 @@ import React, { useState } from 'react';
 import Anecdotes from './Anecdotes';
 
 //Button Component
-const Button = ({ click }) => {
+const Button = ({ click, text }) => {
   return (
     <>
-      <button onClick={click}>next anecdotes</button>
+      <button onClick={click}>{text}</button>
     </>
   )
 }
 
 //App COmponent
 const App = () => {
-  //getting the length
-
+ 
 let [selected, setSelected] = useState(0);
+const arr = new Array(32).fill(0);
+// console.log(arr)
+let [votes, setVotes] = useState(arr);
 
+ //getting the length
 let length;
 const getLength = (l) => {
   length = l;
 }
 
+
+
 const quoteHandler = () => {
-  const rand = Math.floor(Math.random() * length);
-  setSelected(rand);
+  // console.log(length)
+  const randQuote = Math.floor(Math.random() * length);
+  setSelected(randQuote);
+}
+
+const voteHandler = () => {
+  const newArr = [...votes];
+  console.log(votes)
+  newArr[selected] += 1;
+  setVotes(newArr);
 }
 
 return (
   <>
    <Anecdotes sendLength={getLength} rand={selected}/>
-    <Button click={quoteHandler} />
+  <p>has {votes[selected]} votes</p>
+  <Button text="vote" click={voteHandler} />
+    <Button text="next anecdotes" click={quoteHandler} />
   </>
 )
 }
